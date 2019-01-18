5c31080fdcc70c32014db236bc34a6b7593bcb1e
Michael Karén <michael.karen@gmail.com>
2019-01-18 10:56:55 +0100

    Update polyfills.ts to fix stackblitz error

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,3 +78,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'core-js/es7/reflect';
